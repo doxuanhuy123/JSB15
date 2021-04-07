dd238b76ac5b40a623f06ae9711fabadb7ab7455
doxuanhuy123 <80506104+doxuanhuy123@users.noreply.github.com>
2021-04-07 20:41:17 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,24 @@
+let bk = prompt("bán kinh")
+let dk = bk*2
+let chuvi = dk*3.14
+console.log("chu vi hình tròn"+ chuvi)
+__________________________________________
+let name= prompt("your name")
+let tuổi=prompt("your age")
+let nghề=prompt("your job")
+console.log("Name:" + name)
+console.log("Your age:" + tuổi)
+console.log("Your job:" + nghề)
+__________________________________________
+let s1=prompt("number1")
+let s2=prompt("number2")
+let s3=prompt("number3")
+let s4=prompt("number4")
+let s5=prompt("number5")
+let tổng=parseInt(s1) + parseInt(s2) + parseInt(s3) + parseInt(s4) + parseInt(s5)
+let tích= s1 * s2 * s3 * s4 * s5
+console.log("tổng là" + tổng)
+console.log("tích là" + tích)
+var numbers = [ s1, s2, s3, s4, s5]
+Math.max(parseInt(s1), parseInt(s2), parseInt(s3), parseInt(s4), parseInt(s5))
+Math.min(parseInt(s1), parseInt(s2), parseInt(s3), parseInt(s4), parseInt(s5))
